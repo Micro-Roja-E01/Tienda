01d0bb8ff78dc6c272828c611d7c2625ae8ccd5e
SebastianGalvez02 <167112036+SebastianGalvez02@users.noreply.github.com>
2025-09-27 01:35:42 -0300

    chore(data): se habilitó DbSet de nuevas entidades en el DataContext

--- src/Application/Infrastructure/Data/DataContext.cs
+++ src/Application/Infrastructure/Data/DataContext.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Tienda.src.Application.Domain.Models;
-using Tienda.src.Application.Domain.Models;
 
 namespace Tienda.src.Infrastructure.Data
 {
@@ -21,12 +20,10 @@ namespace Tienda.src.Infrastructure.Data
         public DbSet<Image> Images { get; set; } = null!;
         public DbSet<Category> Categories { get; set; } = null!;
         public DbSet<Brand> Brands { get; set; } = null!;
-        // TODO: Terminar estas entidades y sus relaciones
-        /**
         public DbSet<Order> Orders { get; set; } = null!;
         public DbSet<OrderItem> OrderItems { get; set; } = null!;
         public DbSet<Cart> Carts { get; set; } = null!;
         public DbSet<CartItem> CartItems { get; set; } = null!;
-        public DbSet<VerificationCode> VerificationCodes { get; set; } = null!; **/
+        public DbSet<VerificationCode> VerificationCodes { get; set; } = null!;
     }
 }
