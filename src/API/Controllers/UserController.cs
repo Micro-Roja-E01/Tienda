9dece2772be668e50df620b2cd1dc7bdb44da9cb
SebastianGalvez02 <167112036+SebastianGalvez02@users.noreply.github.com>
2025-09-27 01:31:56 -0300

    feat(controller): se implementó el controlador del usuario

---
+++ src/API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tienda.src.API.Controllers
+{
+    /// <summary>
+    /// Controlador base para las operaciones relacionadas con usuarios.
+    [Route("api/[controller]")] // Define la ruta base para el controlador, sustituyendo [controller] por el nombre del controlador.
+    public class UserController : BaseController
+    {
+        
+    }
+}
