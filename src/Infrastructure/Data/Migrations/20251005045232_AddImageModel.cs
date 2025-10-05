9d72f0933c06ffdc8220fb8942ef3edeaef6830e
SebastianGalvez02 <167112036+SebastianGalvez02@users.noreply.github.com>
2025-10-05 20:28:45 -0300

    feat: Se añadio modelo de imagen y database migration

---
+++ src/Infrastructure/Data/Migrations/20251005045232_AddImageModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tienda.src.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_VerificationCodes_UserId",
+                table: "VerificationCodes",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VerificationCodes_AspNetUsers_UserId",
+                table: "VerificationCodes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VerificationCodes_AspNetUsers_UserId",
+                table: "VerificationCodes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VerificationCodes_UserId",
+                table: "VerificationCodes");
+        }
+    }
+}
