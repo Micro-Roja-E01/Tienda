1bc22e2dbe603734b095027e3b94f543ff41c8ca
SebastianGalvez02 <167112036+SebastianGalvez02@users.noreply.github.com>
2025-11-02 18:26:41 -0300

    feat: se añaden validaciones y sanitizacion HTML

--- src/Application/DTO/ProductDTO/CreteProductDTO.cs
+++ src/Application/DTO/ProductDTO/CreteProductDTO.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Tienda.src.Application.Services.Validators;
 
 namespace Tienda.src.Application.DTO.ProductDTO
 {
@@ -10,11 +11,13 @@ namespace Tienda.src.Application.DTO.ProductDTO
         [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
         [StringLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres.")]
         [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres.")]
+        [SanitizeHtml]
         public required string Title { get; set; }
 
         [Required(ErrorMessage = "La descripción del producto es obligatoria.")]
         [StringLength(100, ErrorMessage = "La descripción no puede tener más de 100 caracteres.")]
         [MinLength(10, ErrorMessage = "La descripción debe tener al menos 10 caracteres.")]
+        [SanitizeHtml]
         public required string Description { get; set; }
 
         [Required(ErrorMessage = "El precio del producto es obligatorio.")]
@@ -24,6 +27,9 @@ namespace Tienda.src.Application.DTO.ProductDTO
         [Required(ErrorMessage = "El stock del producto es obligatorio.")]
         [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo.")]
         public required int Stock { get; set; }
+
+        [Range(0, 100, ErrorMessage = "El descuento debe estar entre 0 y 100.")]
+        public int Discount { get; set; } = 0;
 
         [Required(ErrorMessage = "El estado del producto es obligatorio.")]
         [RegularExpression("^(Nuevo|Usado)$", ErrorMessage = "El estado debe ser 'Nuevo' o 'Usado'.")]
